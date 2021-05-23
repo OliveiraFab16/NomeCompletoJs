5a09b773423f695ddb33d6335fe095debc89693b
oliveirafab16 <fabriciocarvalho932@gmail.com>
2021-05-23 17:56:50 -0300

    NomeCompletoJS

---
+++ JAVINHA.js
@@ -0,0 +1,13 @@
+let Nome = prompt ("Seu primeiro nome por gentileza: ");
+
+let Sobrenome = prompt ("Agora seu sobrenome: ");
+
+let NomeCompleto = `Seu Nome é ? : ${Nome} ${Sobrenome}.`
+
+alert (NomeCompleto);
+
+alert("Olá Bem Vindo ao GitHub")
+
+alert("Agora uma Frase para refletir!");
+alert("“As grandes coisas podem ser reveladas através de pequenos indícios”!");
+
